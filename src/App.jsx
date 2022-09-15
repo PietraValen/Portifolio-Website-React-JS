a74a8b23afd0f4a415cef873ff1c4752ea4caede
Pietra Valentina <100318524+PietraValen@users.noreply.github.com>
2022-09-14 21:57:07 -0300

    second-cm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Header from "./Components/Header/Header";
+import Nav from "./Components/Nav/Nav";
+import About from "./Components/About/About";
+import Experience from "./Components/experience/Experience";
+import Services from "./Components/services/Services";
+import Portifolio from "./Components/portifolio/Portifolio";
+import Testimonials from "./Components/testimonials./Testimonials";
+import Contact from "./Components/contact/Contact";
+import Footer from "./Components/footer/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portifolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
-
 export default App;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Nav.css";
+
+const nav = () => {
+  return <div>nav</div>;
+};
+
+export default nav;
