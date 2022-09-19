ce33928352c17ca8085a1a6d609b86940a361777
Pietra Valentina <100318524+PietraValen@users.noreply.github.com>
2022-09-18 21:00:22 -0300

    files and building update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../src/assets/me.png";
 
 const header = () => {
-  return <div>header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Olá, eu sou</h5>
+        <h1>Pietra Valentina</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="Eu" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default header;
