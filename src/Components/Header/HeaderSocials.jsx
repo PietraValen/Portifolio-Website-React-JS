852a83b9fea4e4a78e50133e37da7a39e37fac92
Pietra Valentina <100318524+PietraValen@users.noreply.github.com>
2022-09-19 22:06:39 -0300

    header-update

---
+++ src/Components/Header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/pietra-valentina/"
+        target="_blank"
+      ></a>
+      <a href="https://github.com/PietraValen" target="_blank"></a>
+      <a href="https://www.instagram.com/pietra.h1/" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
