0da53d25f2927d4f5d501fd2082ba3a05ca3e5c3
PietraValen <100318524+PietraValen@users.noreply.github.com>
2022-09-24 11:28:39 -0300

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./Components/Header/Header";
-import Nav from "./Components/Nav/Nav";
-import About from "./Components/About/About";
-import Experience from "./Components/experience/Experience";
-import Services from "./Components/services/Services";
-import Portifolio from "./Components/portifolio/Portifolio";
-import Testimonials from "./Components/testimonials./Testimonials";
-import Contact from "./Components/contact/Contact";
-import Footer from "./Components/footer/Footer";
-import "./src/index.css";
+import React from 'react';
+import Header from './src/Components/Header/Header.jsx';
+import Nav from './src/Components/Nav/Nav.jsx';
+import About from './src/Components/About/About.jsx';
+import Experience from './src/Components/experience/Experience.jsx';
+import Services from './src/Components/services/Services.jsx';
+import Portifolio from './src/Components/portifolio/Portifolio.jsx';
+import Testimonials from './src/Components/testimonials./Testimonials.jsx';
+import Contact from './src/Components/contact/Contact.jsx';
+import Footer from './src/Components/footer/Footer.jsx';
+import './src/index.css';
 
 const App = () => {
   return (
